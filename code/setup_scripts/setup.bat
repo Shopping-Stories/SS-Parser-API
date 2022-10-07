6c948998b863b761cdb92f2eff7e2a2cf2a5e29c
Mahlon Scott <1kingjms@gmail.com>
2022-10-07 02:13:58 -0400

    Fixed setup scripts again

--- code/setup_scripts/setup.bat
+++ code/setup_scripts/setup.bat
@@ -3,4 +3,5 @@ python -m venv ..\parserEnv
 call ..\parserEnv\Scripts\activate.bat
 pip install -r ..\requirements.txt
 python .\setup.py
+cd ..
 python ..\api_entry.py
--- code/setup_scripts/setup.sh
+++ code/setup_scripts/setup.sh
@@ -4,4 +4,5 @@ python3 -m venv ../parserEnv
 source ../parserEnv/Scripts/activate
 pip install -r ../requirements.txt
 python ./setup.py
+cd ..
 python ../api_entry.py
