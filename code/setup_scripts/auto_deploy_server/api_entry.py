1ce8692c2be5458c3cbc38b67b730efdc90ef814
Mahlon Scott <1kingjms@gmail.com>
2022-10-08 15:14:41 -0400

    prod fix

--- code/setup_scripts/auto_deploy_server/api_entry.py
+++ code/setup_scripts/auto_deploy_server/api_entry.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 from subprocess import Popen
 
 incoming = FastAPI()
@@ -8,6 +7,12 @@ incoming = FastAPI()
 async def main():
     Popen("./redeploy.sh", shell=True)
     return {"message": "Redeploying..."}
+
+@incoming.post("/")
+async def main():
+    Popen("./redeploy.sh", shell=True)
+    return {"message": "Redeploying..."}
+
 
 if __name__ == "__main__":
     # uvicorn.run("api_entry:incoming", port=5050, log_level='info')
