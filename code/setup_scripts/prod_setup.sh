64e98147faec065d941e619d6ce9eb0db52be2d1
Mahlon Scott <1kingjms@gmail.com>
2022-10-11 22:56:00 -0400

    Prod fix again

--- code/setup_scripts/prod_setup.sh
+++ code/setup_scripts/prod_setup.sh
@@ -6,18 +6,18 @@ pip install --upgrade pip
 pip install -r ../requirements.txt
 python ./setup.py
 sudo apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null 
-if ! gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg | grep 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62; then
-    echo "Warning: Nginx fingerprint incorrect, DO NOT PROCEED with install" >&2
-fi
-echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+# curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+#     | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null 
+# if ! gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg | grep 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62; then
+#     echo "Warning: Nginx fingerprint incorrect, DO NOT PROCEED with install" >&2
+# fi
+# echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+# http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+#     | sudo tee /etc/apt/sources.list.d/nginx.list
+# echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+#     | sudo tee /etc/apt/preferences.d/99nginx
 sudo apt update
-sudo apt -y install nginx
+# sudo apt -y install nginx
 sudo mkdir -p /usr/local/nginx/conf || true
 sudo mkdir -p /etc/nginx || true
 sudo mkdir -p  /usr/local/etc/nginx || true
