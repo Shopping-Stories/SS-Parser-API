4951b6aebaf8ae15251466dae81d907fa704d64a
Mahlon Scott <1kingjms@gmail.com>
2022-10-13 19:48:47 -0400

    Fix redeploying on website to only redeploy on preprod push

--- code/setup_scripts/auto_deploy_server/api_entry.py
+++ code/setup_scripts/auto_deploy_server/api_entry.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from subprocess import Popen
 
 incoming = FastAPI()
@@ -19,9 +19,13 @@ async def gweb():
     return {"message": "Redeploying..."}
 
 @incoming.post("/website")
-async def pweb():
-    Popen("./redeploy_website.sh", shell=True)
-    return {"message": "Redeploying..."}
+async def pweb(request: Request):
+    json = await request.json()
+    if "ref" in json and "preprod" in json["ref"]: 
+        Popen("./redeploy_website.sh", shell=True)
+        return {"message": "Redeploying..."}
+    else:
+        return {"message": "Not redeploying, not preprod branch."}
 
 
 if __name__ == "__main__":
