7263ee98ac079537697b13e49ef69022fd72ff95
Mahlon Scott <1kingjms@gmail.com>
2022-11-17 13:35:25 -0500

    Fix setup script for linux/macos

--- code/setup_scripts/setup.sh
+++ code/setup_scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd setup_scripts
 python3 -m venv ../parserEnv
-source ../parserEnv/Scripts/activate
+source ../parserEnv/bin/activate
 pip install -r ../requirements.txt
 python ./setup.py
 python ../api_entry.py
