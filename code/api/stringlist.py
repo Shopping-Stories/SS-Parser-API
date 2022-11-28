067634688992ea285e4858d3de1f703b132cf849
lindseymazurowski <lindseymazurowski@knights.ucf.edu>
2022-11-28 15:21:42 -0500

    stringlist endpoint deployment

--- code/api/stringlist.py
+++ code/api/stringlist.py
@@ -4,40 +4,29 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
-def createStringList(name: str):
+@router.get("/createStringList/{name}")
+async def createStringList(name: str):
   global db
   collection = db["stringLists"]
   strList = {"name": name}
   collection.insert_one(strList)
 
-
-def addString(name: str, string: str):
+@router.get("/addString/")
+async def addString(name: str, string: str):
   global db
   collection = db["stringLists"]
   collection.update_one({"name": name}, {"$addToSet": {"strings": string}})
 
-
-def removeString(name: str, string: str):
+@router.get("/removeString/")
+async def removeString(name: str, string: str):
   global db
   collection = db["stringLists"]
   collection.update_one({"name": name}, {"$pull": {"strings": string}})
 
-# didnt know how we wanted strings retrieved so i did both
-
 # returns "strings" array and contents as just the raw json returned from the search
-def getStrings(name: str):
+@router.get("/getStrings/{name}")
+async def getStrings(name: str):
   global db
   collection = db["stringLists"]
   strlist = collection.find_one({"name": name}, {"strings": 1, "_id": 0})
   return str(strlist)
-
-# returns the different entries in the "strings" array as a python list
-def getStringList(name: str):
-  strings = getStrings(name)
-  strings = strings[strings.index('[')+1:strings.index(']')]
-  strings = strings.split(", ")
-  strlist = []
-  for item in strings:
-    strlist.append(item[1:-1])
-  return strlist
