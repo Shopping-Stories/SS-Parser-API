5b1a8843dca89e1db99c59c5f010da52325f4b98
Georgia Newbery <gnewbery22@gmail.com>
2022-12-09 04:32:23 -0500

    relationships and sorted data added

--- code/api/ssParser/database_test.py
+++ code/api/ssParser/database_test.py
@@ -1,134 +1,167 @@
 from parser import db
 from bson.objectid import ObjectId
+import re
+import json
 
+# add collections
 entries_collection = db.entries
+people_collection = db.people
+item_collection = db.items
 
-test = {
-  "folioRefs": [
-    ""
-  ],
-  "ledgerRefs": [
-    ""
-  ],
-  "tobaccoEntry": "",
-  "entry": "test entry 1",
-  "accountHolder": {
-    "prefix": "",
-    "accountFirstName": "James",
-    "accountLastName": "Edwards",
-    "suffix": "",
-    "profession": "",
-    "location": "",
-    "reference": "",
-    "debitOrCredit": 1,
-  },
-  "meta": {
-    "ledger": "C_1760",
-    "reel": "58",
-    "owner": "John Glassford & Company",
-    "store": "Colchester",
-    "year": "1760",
-    "folioPage": "19",
-    "entryID": "18",
-    "comments": "Jane or Jean?"
-  },
-  "dateInfo": {
-    "day": 7,
-    "month": 7,
-    "year": 1761,
-    "fullDate": {
-      "$date": {
-        "$numberLong": "-6579187200000"
-      }
-    }
-  },
-  "itemEntries": {
-      "perOrder": 0,
-      "percentage": 0,
-        "variants": [
-            ""
-        ],
-        "quantity": 7,
-        "qualifier": "yard",
-        "item": "Test 2",
-        "category": "Textiles",
-        "subcategory": "Fabric",
-        "unitCost": {
-            "pounds": 0,
-            "shilling": 0,
-            "pence": 0
-        },
-        "itemCost": {
-            "pounds": 0,
-            "shilling": 7,
-            "pence": 7
-        },
-    "itemsMentioned": [] #maybe delete
-    },
+# sample parser data as dict
+test_parser = {
+  "item": "Test5",
+  "type": "Liber",
   "people": [
+    "Thomas Hanam",
+    "Jacob Smith"
+  ],
+  "liber_book": "A",
+  "phrases": [
     {
-      "name": "Jean Shields"
+      "modifies": "making",
+      "phrase": [
+        "for",
+        "making",
+        "a",
+        "Desk"
+      ]
     }
   ],
-  "places": [],
-  "money": {
-    "quantity": "",
-    "commodity": "",
-    "colony": "Virginia",
-    "sterling": {
-      "pounds": 0,
-      "shilling": 0,
-      "pence": 0
-    },
-    "currency": {
-      "pounds": 1,
-      "shilling": 2,
-      "pence": 3
-    }
-  },
-  "__v": 0
+  "currency_type": "Both",
+  "currency_totaling_contextless": "false", #false put in quotations
+  "commodity_totaling_contextless": "false", #false put in quotations
+  "account_name": "Joe Mama3",
+  "reel": 58,
+  "store_owner": "John Glassford & Company",
+  "folio_year": "1760/1761",
+  "folio_page": 70,
+  "entry_id": "2",
+  "Date Year": "1760",
+  "Folio Reference": "131",
+  "debit_or_credit": "Dr",
+  "context": [
+    [
+      "To",
+      "TRANS",
+      "TO"
+    ],
+    [
+      "Balance",
+      "",
+      "VB"
+    ],
+    [
+      "from",
+      "",
+      "IN"
+    ],
+    [
+      "Liber A",
+      "LIBER",
+      "NNP"
+    ]
+  ],
+  "pounds": 0,
+  "shillings": 7,
+  "pennies": 0,
+  "farthings": 0,
+  "pounds_ster": 2,
+  "shillings_ster": 10,
+  "pennies_ster": 0,
+  "Commodity": "Tobacco",
+  "Quantity": 35
 }
 
-# exact search for people (accountHolder specifically), add accountHolderID to dict
-account_holder = test["accountHolder"]
-first_name = account_holder["accountFirstName"]
-last_name = account_holder["accountLastName"]
+#functions
+## exact search for account_holder in people, add accountHolderID to entries
+## checks if already in db, adds to db if not, makes relationship regardless
+def account_holder_rel(test_parser):
+  if people_collection.count_documents({"name": test_parser["account_name"]}, limit = 1):
+    people_id = (people_collection.find_one({"name": test_parser["account_name"]}, {"_id"}))
+    test_parser.update({"accountHolderID": people_id["_id"]})
+  else :
+    people_id = people_collection.insert_one({"name": test_parser["account_name"]}).inserted_id
+    test_parser.update({"accountHolderID": people_id})
+  del test_parser["account_name"] 
+  return
 
-people_collection = db.people
-## print(people_collection.find_one({"firstName": "James", "lastName": "Edwards"}, {"_id"}))
-if db.people_collection.count_documents({"firstName": first_name, "lastName": last_name}, limit = 1):
-  people_id = (people_collection.find_one({"firstName": first_name, "lastName": last_name}, {"_id"}))
-  test["accountHolder"].update({"accountHolderID": people_id})
-else :
-  people_id = people_collection.insert_one(test["accountHolder"]).inserted_id
-  test["accountHolder"].update({"accountHolderID": people_id})
+## exact search for item in items, add itemID to dict
+## checks if already in db, adds to db if not, makes relationship regardless
+def item_rel(test_parser):
+  if item_collection.count_documents({"item": test_parser["item"]}, limit = 1) > 0:
+    item_id = (item_collection.find_one({"item": test_parser["item"]}, {"_id"}))
+    test_parser.update({"itemID": item_id["_id"]})
+  else:
+    item_id = item_collection.insert_one({"item": test_parser["item"]}).inserted_id
+    test_parser.update({"itemID": item_id})
+  del test_parser["item"]  
+  return
 
-# exact search for items, add itemID to dict
-item_entries = test["itemEntries"]
-item = item_entries["item"]
-print(item)  
+## exact search for people array in people, add peopleID(s) to dict
+## loops through array of people, checks if already in db, adds to db if not, makes relationship regardless
+def people_rel(test_parser):
+  test_parser.update({"peopleID": []})
+  for person in test_parser["people"]:
+    if people_collection.count_documents({"name": person}, limit = 1):
+      people_id = (people_collection.find_one({"name": person}, {"_id"}))
+      test_parser["peopleID"].append(people_id["_id"])
+    else :
+      people_id = people_collection.insert_one({"name": person}).inserted_id
+      test_parser["peopleID"].append(people_id)
+  del test_parser["people"]
+  return
 
-item_collection = db.items
-if db.items_collection.count_documents({"item": item}, limit = 1):
-    item_id = (item_collection.find_one({"item": item}, {"_id"}))
-    test["itemEntries"].update({"itemID": item_id})
-else:
-    item_id = item_collection.insert_one(test["itemEntries"]).inserted_id
-    test["itemEntries"].update({"itemID": item_id})
+## puts specified values (keys) into an object (type) together for entries
+## what can go wrong: if input does not contain all keys
+def create_object(test_parser, keys, type):
+  for key in keys:
+    if key not in test_parser:
+      print("does not contain all keys\n")
+      return
+  object = test_parser.fromkeys(keys)
+  for key in keys:
+    object.update({key: test_parser[key]})
+    del test_parser[key]
+  test_parser.update({type: object})
+  return
 
-# add dict to database
-test_id = entries_collection.insert_one(test).inserted_id
-print(test_id)
+# main
+## ensure that keys exist, then create relationships
+if "account_name" in test_parser:
+  account_holder_rel(test_parser)
+if "item" in test_parser:
+  item_rel(test_parser)
+if "people" in test_parser:
+  people_rel(test_parser)
+
+## create objects to group together similar data
+## define keys, change key values to change which variables are grouped
+## what can go wrong: will not create object if not all specified keys are in the input  
+currency_keys = ["pounds", "shillings", "pennies", "farthings"]
+sterling_keys = ["pounds_ster", "shillings_ster", "pennies_ster"]
+ledger_keys = ["reel", "folio_year", "folio_page", "entry_id"]
+create_object(test_parser, currency_keys, "currency")
+create_object(test_parser, sterling_keys, "sterling")
+create_object(test_parser, ledger_keys, "ledger")
+
+## add dict to database
+#print(test_parser) # prints final entry to terminal 
+test_id = entries_collection.insert_one(test_parser).inserted_id
+#print(test_id) # prints entryID to terminal
 
 #NEED
+## format currency
 ## fuzzy search for items
 ## accountHolder relationship
+## parent/child data placeholders people & items
 ## items relationship
 ## tobaccoMarks relationship
 ## places relarionship
 ## people relationship
 ## combine items and categories
 
-#"accountHolderID": {
-#      "$oid": "61788aa24e984cc095983aa2"
-#    }
+# partital item search for parent/child ---- in progress
+#if db.items_collection.count_documents({"item": {$regex: item}}) > 0:
+
+#{_id: ObjectId('')}
