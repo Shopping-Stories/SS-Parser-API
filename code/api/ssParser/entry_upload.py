211aee1d5c09c616884d4e5f2440f0c327ced7df
lindseymazurowski <lindseymazurowski@knights.ucf.edu>
2023-01-27 17:18:07 -0500

    Updated parser to create search terms after new entries are inserted

--- code/api/ssParser/entry_upload.py
+++ code/api/ssParser/entry_upload.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel, Field
 from json import load
 from os import listdir
 from os.path import join, dirname
+from ..fuzzysearch import createDmetasForEntries
 
 # add collections
 entries_collection = db.entries
@@ -276,6 +277,8 @@ def insert_parsed_entries(parsed_entry: POutputList, background_tasks: Backgroun
     try:
         # Add all to database
         result = entries_collection.insert_many(new_entries)
+        # Create search terms for new entries
+        background_tasks.add_task(createDmetasForEntries, result.inserted_ids)
     except Exception as e:
         # Return any errors that may happen
         return Message(message="ERROR: " + format_exc, error=True)
